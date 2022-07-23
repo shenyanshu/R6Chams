8c87a91aecf9ffc4a5113b8e80f699c5768277ed
Emilyy <78696700+EmilyButGay@users.noreply.github.com>
2022-07-23 19:45:42 +0300

    Update DllMain.cpp

--- Internal/DllMain.cpp
+++ Internal/DllMain.cpp
@@ -6,7 +6,6 @@
 #include <vector>
 #include <d3d11.h>
 #include <dxgi.h>
-#include <D3DX11tex.h>
 #include <D3Dcompiler.h>
 #pragma comment(lib, "D3dcompiler.lib")
 #pragma comment(lib, "d3d11.lib")
@@ -42,6 +41,13 @@ D3D11DrawIndexedInstancedIndirectHook phookD3D11DrawIndexedInstancedIndirect = N
 
 typedef LRESULT(CALLBACK* WNDPROC)(HWND, UINT, WPARAM, LPARAM);
 typedef uintptr_t PTR;
+
+ID3D11Device* pDevice = NULL;
+ID3D11DeviceContext* pContext = NULL;
+
+DWORD_PTR* pSwapChainVtable = NULL;
+DWORD_PTR* pContextVTable = NULL;
+DWORD_PTR* pDeviceVTable = NULL;
 
 //create texture
 ID3D11Texture2D* texGreen = nullptr;
@@ -262,22 +268,91 @@ void __stdcall hookD3D11DrawIndexedInstancedIndirect(ID3D11DeviceContext* pConte
 	return phookD3D11DrawIndexedInstancedIndirect(pContext, pBufferForArgs, AlignedByteOffsetForArgs);
 }
 
+LRESULT CALLBACK DXGIMsgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) { return DefWindowProc(hwnd, uMsg, wParam, lParam); }
 DWORD WINAPI InitiateChams()
 {
+	HMODULE hDXGIDLL = 0;
 	do
 	{
-		Sleep(100);
-	} while (!GetModuleHandleA("d3d11.dll"));
+		hDXGIDLL = GetModuleHandleA("dxgi.dll");
+		Sleep(4000);
+	} while (!hDXGIDLL);
+	Sleep(100);
 
-	uintptr_t DrawIndexedFunction = uintptr_t(GetModuleHandleA("d3d11.dll")) + 0x166950;
-	uintptr_t Vtable = uintptr_t(GetModuleHandleA("d3d11.dll")) + 0x1CE1A0;
+	IDXGISwapChain* pSwapChain;
 
-	DWORD dwOld;
-	VirtualProtect((void*)Vtable, 1, PAGE_EXECUTE_READWRITE, &dwOld);
-	*(void**)Vtable = &hookD3D11DrawIndexedInstancedIndirect;
-	VirtualProtect((void*)Vtable, 1, dwOld, &dwOld);
+	WNDCLASSEXA wc = { sizeof(WNDCLASSEX), CS_CLASSDC, DXGIMsgProc, 0L, 0L, GetModuleHandleA(NULL), NULL, NULL, NULL, NULL, "DX", NULL };
+	RegisterClassExA(&wc);
+	HWND hWnd = CreateWindowA("DX", NULL, WS_OVERLAPPEDWINDOW, 100, 100, 300, 300, NULL, NULL, wc.hInstance, NULL);
 
-	phookD3D11DrawIndexedInstancedIndirect = (D3D11DrawIndexedInstancedIndirectHook)DrawIndexedFunction;
+	D3D_FEATURE_LEVEL requestedLevels[] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_1 };
+	D3D_FEATURE_LEVEL obtainedLevel;
+	ID3D11Device* d3dDevice = nullptr;
+	ID3D11DeviceContext* d3dContext = nullptr;
+
+	DXGI_SWAP_CHAIN_DESC scd;
+	ZeroMemory(&scd, sizeof(scd));
+	scd.BufferCount = 1;
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	scd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+
+	scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	scd.OutputWindow = hWnd;
+	scd.SampleDesc.Count = 1;
+	scd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	scd.Windowed = ((GetWindowLongPtr(hWnd, GWL_STYLE) & WS_POPUP) != 0) ? false : true;
+
+	// LibOVR 0.4.3 requires that the width and height for the backbuffer is set even if
+	// you use windowed mode, despite being optional according to the D3D11 documentation.
+	scd.BufferDesc.Width = 1;
+	scd.BufferDesc.Height = 1;
+	scd.BufferDesc.RefreshRate.Numerator = 0;
+	scd.BufferDesc.RefreshRate.Denominator = 1;
+
+	UINT createFlags = 0;
+#ifdef _DEBUG
+	// This flag gives you some quite wonderful debug text. Not wonderful for performance, though!
+	createFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	if (FAILED(D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		createFlags,
+		requestedLevels,
+		sizeof(requestedLevels) / sizeof(D3D_FEATURE_LEVEL),
+		D3D11_SDK_VERSION,
+		&scd,
+		&pSwapChain,
+		&pDevice,
+		&obtainedLevel,
+		&pContext)
+	))
+	{
+		return false;
+	}
+
+	pSwapChainVtable = (DWORD_PTR*)pSwapChain;
+	pSwapChainVtable = (DWORD_PTR*)pSwapChainVtable[0];
+
+	pContextVTable = (DWORD_PTR*)pContext;
+	pContextVTable = (DWORD_PTR*)pContextVTable[0];
+
+	pDeviceVTable = (DWORD_PTR*)pDevice;
+	pDeviceVTable = (DWORD_PTR*)pDeviceVTable[0];
+
+	if (MH_Initialize() != MH_OK) { return 1; }
+	if (MH_CreateHook((DWORD_PTR*)pContextVTable[39], hookD3D11DrawIndexedInstancedIndirect, reinterpret_cast<void**>(&phookD3D11DrawIndexedInstancedIndirect)) != MH_OK) { return 1; }
+	if (MH_EnableHook((DWORD_PTR*)pContextVTable[39]) != MH_OK) { return 1; }
+
+	pDevice->Release();
+	pContext->Release();
+	pSwapChain->Release();
+
+	DestroyWindow(hWnd);
 
 	return true;
 }
